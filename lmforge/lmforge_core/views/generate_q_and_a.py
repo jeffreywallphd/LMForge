05f7abdc21a376fcfad17bf18ffa9eece72b2e8b
Yaman Aljnadi <60922667+yaman-aljnadi@users.noreply.github.com>
2025-11-14 21:02:38 -0500

    Update generate_q_and_a.py

--- lmforge/lmforge_core/views/generate_q_and_a.py
+++ lmforge/lmforge_core/views/generate_q_and_a.py
@@ -76,7 +76,7 @@ def get_model_and_tokenizer():
         return MODEL, TOKENIZER, device  
 
     try:
-        model_name = "Qwen/Qwen2.5-7B-Instruct"  
+        model_name = "#"  
 
         TOKENIZER = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
         if TOKENIZER.pad_token is None:
