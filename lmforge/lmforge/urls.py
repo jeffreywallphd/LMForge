6ad2838957fb143c1806a03d268939b355482e1b
Divy Patel <125264776+Divy0409@users.noreply.github.com>
2026-02-04 03:31:23 -0500

    minor UI fix

--- lmforge/lmforge/urls.py
+++ lmforge/lmforge/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path,include
+import os
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -29,3 +30,6 @@ urlpatterns = [
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'lmforge', 'static'))
